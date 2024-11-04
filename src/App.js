a8b495b60bc05ee2a4448c49aad0cae817cc5391
ShamitrM <shamitr.mardikar@gmail.com>
2024-11-04 11:47:30 +0530

    Image borken
    Imported Image but only shows the thumbnail icon along with Alt Id.
    @rectangle-man pls fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
 import NavBar from './Navbar';
+import './Background.css'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <img
+      src='public/Placeholder_Img.png'
+      alt='background'
+      className='Backgroundimg'
+      ></img>
     </div>
   );
 }
